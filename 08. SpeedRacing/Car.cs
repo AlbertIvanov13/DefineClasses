5e0285867d521095ba47f1079089419029dd4da1
Albert Ivanov <albertivanov187@gmail.com>
2025-02-21 00:06:16 +0200

    100/100

---
+++ 08. SpeedRacing/Car.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+		private string model;
+		private double fuelAmount;
+		private double fuelConsumptionPerKilometer;
+		private double travelledDistance;
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        {
+			this.Model = model;
+			this.fuelAmount = fuelAmount;
+			this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+        }
+        public Car(string model)
+        {
+            this.Model = model;
+        }
+
+        public Car()
+        {
+            
+        }
+
+        public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+
+
+		public double FuelAmount
+		{
+			get { return fuelAmount; }
+			set { fuelAmount = value; }
+		}
+
+
+		public double FuelConsumptionPerKilometer
+        {
+			get { return fuelConsumptionPerKilometer; }
+			set { fuelConsumptionPerKilometer = value; }
+		}
+
+
+		public double TravelledDistance
+		{
+			get { return travelledDistance; }
+			set { travelledDistance = value; }
+		}
+
+		public void Drive(Dictionary<string, Car> cars, string model, double amountOfKm)
+		{
+            foreach (var car in cars.Keys)
+            {
+                if (car == model)
+                {
+                    if (cars[car].FuelAmount - cars[car].FuelConsumptionPerKilometer * amountOfKm >= 0)
+                    {
+                        cars[car].FuelAmount -= cars[car].FuelConsumptionPerKilometer * amountOfKm;
+                        cars[car].TravelledDistance += amountOfKm;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Insufficient fuel for the drive");
+                        break;
+                    }
+                }
+            }
+		}
+	}
+}
---
+++ 08. SpeedRacing/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace SpeedRacing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Car> cars = new Dictionary<string, Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] carInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = carInfo[0];
+                double fuelAmount = double.Parse(carInfo[1]);
+                double fuelConsumption = double.Parse(carInfo[2]);
+                Car car = new Car(model, fuelAmount, fuelConsumption);
+
+                if (!cars.ContainsKey(model))
+                {
+                    cars.Add(model, car);
+                }
+            }
+
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                string[] tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = tokens[1];
+                double amountOfKm = double.Parse(tokens[2]);
+
+                Car car = new Car();
+
+                car.Drive(cars, model, amountOfKm);
+                
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var item in cars)
+            {
+                Console.WriteLine($"{item.Key} {item.Value.FuelAmount:f2} {item.Value.TravelledDistance}");
+            }
+        }
+    }
+}
