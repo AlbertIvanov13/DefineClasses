0cb85b6b8bd9c760e4ee9f4cd1cef05506cea74c
Albert Ivanov <albertivanov187@gmail.com>
2025-02-20 17:29:35 +0200

    100/100

---
+++ 07. DateModifier/DateModifier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        private string startDate;
+        private string endDate;
+
+        public DateModifier(string startDate, string endDate)
+        {
+            this.StartDate = startDate;
+            this.EndDate = endDate;
+        }
+
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+
+        public int CalculateDatesDifference(string startDate, string endDate)
+        {
+            TimeSpan timespan = DateTime.Parse(startDate) - DateTime.Parse(endDate);
+
+            return Math.Abs(timespan.Days);
+        }
+    }
+}
---
+++ 07. DateModifier/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DateModifier
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string startDate = Console.ReadLine();
+            string endDate = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier(startDate, endDate);
+
+            Console.WriteLine(dateModifier.CalculateDatesDifference(startDate, endDate));
+        }
+    }
+}
