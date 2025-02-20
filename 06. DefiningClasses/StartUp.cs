c4a58a86fc03398dc83e44a4b64f0fb14f61f6c1
Albert Ivanov <albertivanov187@gmail.com>
2025-02-20 15:34:53 +0200

    Update

---
+++ 06. DefiningClasses/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person person1 = new Person();
+
+            Console.WriteLine($"{person1.Name} {person1.Age}");
+
+            Person person2 = new Person("George", 18);
+
+            Console.WriteLine($"{person2.Name} {person2.Age}");
+
+            Person person3 = new Person("Jose", 43);
+
+            Console.WriteLine($"{person3.Name} {person3.Age}");
+
+        }
+    }
+}
